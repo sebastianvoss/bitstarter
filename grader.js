4bc7eee38bcf1bb4ee5a6f81b6938896f31246a7
Sebastian Voss <voss@dev24.de>
2013-07-06 08:53:06 +0000

    Enhanced grader to read from URLs

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,16 +37,24 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function(html) {
+    return cheerio.load(html);
 };
+
+var retrieveFromFile = function(file, callback) {
+    fs.readFile(file, callback);
+}
+
+var retrieveFromUrl = function(url, callback) {
+    rest.get(url).on('complete', callback);
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(html, checksfile) {
+    $ = cheerioHtmlFile(html);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +74,25 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var runCheck = function(html) {
+	var checkJson = checkHtml(html, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+
+    if(program.url) {
+	retrieveFromUrl(program.url, function(result, response) {
+	    runCheck(result);
+	});
+    } else if(program.file) {
+	retrieveFromFile(program.file, function(err, data) {
+	    runCheck(data);
+	});
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
