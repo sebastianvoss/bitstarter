dba4abc13d23b96b53ba0afd52fa397a9e718abc
sebastianvoss <voss@dev24.de>
2013-07-19 17:10:06 +0000

    Responsive design

--- web.js
+++ web.js
@@ -4,14 +4,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = fs.readFileSync("index.html");
+var buffer = fs.readFileSync("index2.html");
 var content = buffer.toString();
 
 app.get('/', function(request, response) {
   response.send(content);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
